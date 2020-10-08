323722d2e36d4049b269825207e60d9f628e5aaf
Lianne Allen-Jacobson <LianneJacobson@gmail.com>
2020-10-08 14:15:38 -0400

    Initial commit to our app

---
+++ client/build/precache-manifest.ab0fb0e1a92ad8df853f0e2403dbae0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0049030001b9747fd712ceb44ab4dda3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae655f74c49eb72cab85",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bd3d21fb6dce97b98a08",
+    "url": "/static/js/2.6f1ab550.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6f1ab550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae655f74c49eb72cab85",
+    "url": "/static/js/main.b4b0e717.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
